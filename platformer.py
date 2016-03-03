d2216c97664a379d2a7b3911966155e2c5eb405c
JuddLunt <s067257080@ocdsb>
2016-03-03 08:57:57 -0500

    Set a Background
    Hey, it's a start

--- platformer.py
+++ platformer.py
@@ -3,12 +3,21 @@ from pygame.locals import *
 from Arcade import arcade
 from colours import *
 
-def game():
-    
+def game(arcade):
 
+    
+    background = pygame.Surface((800,800))
+    background.fill(grey,(0,0,800,800))
+    arcade.initBackground(background)
+    while True:
+        arcade.getEvents()
+        
+        arcade.drawBackground(background)
+
+    
 if __name__ == '__main__':
     pygame.init()
     pygame.mixer.init(22050,-16,2,1024)
     pygame.display.set_caption(os.path.basename(__file__).split('.')[0])
-    game()
+    game(arcade())
 
